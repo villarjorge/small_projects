c33d023a7b6679a3f9190e95927fb0b8ca1c14f4
Jorge San José Villar <villarjorgesanjosedc@gmail.com>
2022-01-10 16:13:28 +0100

    Added function that selects a random point in a circle

--- colorblind_generator/circles_in_circle.py
+++ colorblind_generator/circles_in_circle.py
@@ -42,28 +42,35 @@ def get_array_from_image():
     img = Image.open("map_colorblind.png")
     return np.array(img)
 
+# Invisible circle in which all the circles are going to live
+big_circle = Circle((450, 450), 400, "white")
+def get_random_point_within_circle():
+    """Gets a random point within a circle, without strange russel paradox shenanigans 
+    Mathstackschange: https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly
+    """
+    r = big_circle.radius*np.sqrt(rng.random())
+    theta = 2*np.pi*rng.random()
+    return (big_circle.position[0] + r*np.cos(theta), big_circle.position[1] + r*np.sin(theta))
+
 def main_init():
 
     # This this are the parameters that you want to change
-    number_of_circles = 1000
-    current_radius = 10
+    number_of_circles = 3000
+    current_radius = 7
     # Minimum radius, the program is going to reduce the radius until this number is reached
-    minimum_radius = 2
+    minimum_radius = 1
     # How many times it tries to find a suitable position (so that the circles do not overlap)
     max_fails = 500
 
     # Generate the circle objects
     circle_obj_list = list()
-    # Invisible circle in which all the circles are going to live
-    big_circle = Circle((450, 450), 400, "white")
+    
     # Count how many times do we fail
     fails = 0
 
     while number_of_circles > 0:
         # Choose a random point within the big circle
-        r = big_circle.radius*np.sqrt(rng.random())
-        theta = 2*np.pi*rng.random()
-        random_point = (big_circle.position[0] + r*np.cos(theta), big_circle.position[1] + r*np.sin(theta))
+        random_point = get_random_point_within_circle()
         new_circle = Circle(
             position=random_point,
             radius=current_radius, #radius=rng.integers(10, 15), 
@@ -101,7 +108,7 @@ def main_init():
         # If we go to the image provided and there is black, the circle will be another color
         # We suppose that there is only black and white in the image
         if img_array[int(circle.position[1])][int(circle.position[0])][0] == 0:
-            circle.color = "#B5CA9D"
+            circle.color = "#000000" #"#B5CA9D"
     print("Colors updated")
     print(f" Final number of circles: {len(circle_obj_list)}")
     return circle_obj_list
